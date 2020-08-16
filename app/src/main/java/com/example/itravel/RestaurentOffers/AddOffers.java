690bcea113560bd954b4753a5004770b3395dfb5
DiniduRathnayaka <ds.rathnayaka98@gmail.com>
2020-08-16 23:10:26 +0530

    change CSS pages

--- app/src/main/java/com/example/itravel/RestaurentOffers/AddOffers.java
+++ app/src/main/java/com/example/itravel/RestaurentOffers/AddOffers.java
@@ -2,16 +2,30 @@ package com.example.itravel.RestaurentOffers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.itravel.R;
 
 public class AddOffers extends AppCompatActivity {
 
+    Button addbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_offers);
+
+        addbtn=(Button) findViewById(R.id.addOffer);
+        addbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AddOffers.this,Hotels.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }

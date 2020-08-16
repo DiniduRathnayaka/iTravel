ae0753c6313cac0663581fa12d8707678a6a6485
DiniduRathnayaka <ds.rathnayaka98@gmail.com>
2020-08-17 04:17:24 +0530

    Tiran new UI

---
+++ app/src/main/java/com/example/profile_upgrade/PaymentDetailsUpdate.java
@@ -0,0 +1,17 @@
+package com.example.profile_upgrade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itravel.R;
+
+public class PaymentDetailsUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_details_update);
+
+    }
+}
---
+++ app/src/main/java/com/example/profile_upgrade/Viewpaymentlogs.java
@@ -0,0 +1,16 @@
+package com.example.profile_upgrade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itravel.R;
+
+public class Viewpaymentlogs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewpaymentlogs);
+    }
+}

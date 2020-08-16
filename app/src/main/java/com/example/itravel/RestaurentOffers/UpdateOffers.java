f66ec845e4721d9243a1883dd37622c92cb98d33
DiniduRathnayaka <ds.rathnayaka98@gmail.com>
2020-08-16 19:28:34 +0530

    Restaurent Offers new Function added

---
+++ app/src/main/java/com/example/itravel/RestaurentOffers/UpdateOffers.java
@@ -0,0 +1,14 @@
+package com.example.itravel.RestaurentOffers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdateOffers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_offers);
+    }
+}

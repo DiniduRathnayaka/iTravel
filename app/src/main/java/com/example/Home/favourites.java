64e3c8d40959177e56e832c2a4dd5f47a3145d6f
Tiran98 <49815897+Tiran98@users.noreply.github.com>
2020-08-17 06:17:00 +0530

    Tiran - Home non functioning interface

---
+++ app/src/main/java/com/example/Home/favourites.java
@@ -0,0 +1,16 @@
+package com.example.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.itravel.R;
+
+public class favourites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites);
+    }
+}

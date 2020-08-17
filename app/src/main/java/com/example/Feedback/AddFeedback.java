fb203f409ed136863b571d8b5c44fe65d89db0b8
Tiran98 <49815897+Tiran98@users.noreply.github.com>
2020-08-17 12:17:30 +0530

    Tiran - done project

--- app/src/main/java/com/example/Feedback/AddFeedback.java
+++ app/src/main/java/com/example/Feedback/AddFeedback.java
@@ -1,12 +1,20 @@
 package com.example.Feedback;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 import com.example.itravel.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class AddFeedback extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +22,16 @@ public class AddFeedback extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_feedback);
 
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
     }
 }
--- app/src/main/java/com/example/itravel/RestaurentOffers/AddOffers.java
+++ app/src/main/java/com/example/itravel/RestaurentOffers/AddOffers.java
@@ -1,6 +1,9 @@
 package com.example.itravel.RestaurentOffers;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,8 +13,13 @@ import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.itravel.R;
+import com.google.android.material.navigation.NavigationView;
 
 public class AddOffers extends AppCompatActivity {
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
 
     Button addbtn;
 
@@ -19,6 +27,17 @@ public class AddOffers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_offers);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
 
         addbtn=(Button) findViewById(R.id.addOffBtn);
         addbtn.setOnClickListener(new View.OnClickListener() {
